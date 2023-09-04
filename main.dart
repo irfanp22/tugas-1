02ff72032597389f3d14881f460be571e3c8ef27
irfanp22 <106128747+irfanp22@users.noreply.github.com>
2023-09-04 22:22:38 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,14 +2,18 @@ class Mahasiswa {
   String nim;
   String nama;
   int tahunLahir;
-
-  Mahasiswa(this.nim, this.nama, this.tahunLahir);
+  late int usia;
+  
+  Mahasiswa(this.nim, this.nama, this.tahunLahir){
+    usia = DateTime.now().year - tahunLahir;
+  }
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021043", "Irfan Priatna", 2003);
+  mhs.perkenalan();
 }
